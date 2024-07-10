6bb4778c87c476a55efbe848a6830b76ad93266e
kaustubdutt <kaustubdutt@gmail.com>
2024-07-10 01:47:54 -0400

    july 9th

---
+++ 1598.py
@@ -0,0 +1,16 @@
+from typing import List
+class Solution:
+    def minOperations(self, logs: List[str]) -> int:
+        stack = []
+        for log in logs:
+            if log == "../":
+                if stack:
+                    stack.pop()
+            elif log != "./":
+                stack.append(log)
+        return len(stack)
+
+
+logs = ["d1/", "d2/", "../", "d21/", "./"]
+solution = Solution()
+print(solution.minOperations(logs))
