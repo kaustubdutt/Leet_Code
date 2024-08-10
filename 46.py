e54fb7fdfa5f1fc43858e371b950ed54761eb1b8
kaustubdutt <kaustubdutt@gmail.com>
2024-08-10 02:05:46 -0400

    Permutations

---
+++ 46.py
@@ -0,0 +1,21 @@
+from typing import List
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        self.permutations([], nums, result)
+        return result
+
+    def permutations(self, current: List[int], remaining: List[int], r: List[List[int]]) -> None:
+        if not remaining:
+            r.append(current)
+            return
+        for i in range(len(remaining)):
+            new_current = current + [remaining[i]]
+            new_remaining = remaining[:i] + remaining[i + 1:]
+            self.permutations(new_current, new_remaining, r)
+
+# Example usage
+sol = Solution()
+answer = sol.permute([1, 2, 3, 4])
+print(answer)
